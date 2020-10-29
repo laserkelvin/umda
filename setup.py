c71a3fbd7461caafbfc7799cd0d79b5a675f6a32
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2020-10-29 10:56:07 -0400

    Cleaned up old default folders

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='umda',
     packages=find_packages(),
     version='0.1.0',
     description='Applying cheminformatics to molecular astrophysics',
--- src/pipeline/make_dataset.py
+++
@@ -1,7 +0,0 @@
-
-"""
-make_dataset.py
-
-This module will implement methods to synthesize the
-data used for the project.
-"""
